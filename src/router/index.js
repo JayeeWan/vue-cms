608da268636abfc7df9b6406857c3f5fae8ae0b8
Jayee <JayeeWan@163.com>
2018-11-22 18:52:25 +0800

    news已完成

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,24 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
-import HomeComponent from '../pages/home/'
-import MemberComponent from '../pages/member/'
-import ShopcarComponent from '../pages/shopcar/'
-import SearchComponent from '../pages/search/'
+import homeComponent from '../pages/home/'
+import memberComponent from '../pages/member/'
+import shopcarComponent from '../pages/shopcar/'
+import searchComponent from '../pages/search/'
+import newsListComponent from '../pages/newsList/'
+import newsInfoComponent from '../pages/newsInfo/'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     { path: '/', redirect: '/home' },
-    { path: '/home', component: HomeComponent },
-    { path: '/member', component: MemberComponent },
-    { path: '/shopcar', component: ShopcarComponent },
-    { path: '/search', component: SearchComponent },
+    { path: '/home', component: homeComponent },
+    { path: '/member', component: memberComponent },
+    { path: '/shopcar', component: shopcarComponent },
+    { path: '/search', component: searchComponent },
+    { path: '/home/newsList', component: newsListComponent },
+    { path: '/home/newsInfo/:id', component: newsInfoComponent },
   ],
   linkActiveClass: 'mui-active'
 })
